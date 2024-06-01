65b99fb0e0944337fe86aaa2ce2925310f9b4a2d
杨文枝 <313097973@qq.com>
2024-06-01 18:46:18 +0800

    fix: optimize queryTitle

---
+++ src/Model/HasPublished.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Ella123\HyperfUtils\Model;
+
+use Carbon\Carbon;
+use Hyperf\Database\Model\Builder;
+
+/**
+ * @property int $published 是否发布
+ * @property Carbon $published_at 发布时间
+ * @method static static|Builder queryPublished(?string $published_at)
+ * @method static static|Builder queryPublishedExpired(?string $published_at, ?string $expired_at)
+ */
+trait HasPublished
+{
+    public function scopeQueryPublished(
+        Builder $query,
+        ?string $published_at = null
+    ): void {
+        $query->where(static::getTableName() . '.published', true)
+            ->where(static::getTableName() . '.published_at', '<=', Carbon::parse($published_at));
+    }
+
+    public function scopeQueryPublishedExpired(
+        Builder $query,
+        ?string $published_at = null,
+        ?string $expired_at = null
+    ): void {
+        /** @var static $query */
+        $query->queryPublished($published_at)
+            ->where(function ($query) use ($expired_at) {
+                $query->where(static::getTableName() . '.expired_at', '>=', Carbon::parse($expired_at))
+                    ->orWhereNull(static::getTableName() . '.expired_at');
+            });
+    }
+}
