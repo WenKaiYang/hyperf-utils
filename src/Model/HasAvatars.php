3f4dd782eace80be826e349fc16705b50dc3816e
杨文枝 <313097973@qq.com>
2024-06-25 09:28:58 +0800

    fix: repair HasAvatars

--- src/Model/HasAvatars.php
+++ src/Model/HasAvatars.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace Ella123\HyperfUtils\Model;
 
 use Ella123\HyperfGenerateAvatar\AvatarUtils;
+
 use function Ella123\HyperfUtils\isURL;
 
 /**
@@ -29,12 +30,12 @@ trait HasAvatars
 
     public function gravatar(int $size = 100, ?string $username = null): string
     {
-        !$username && $username = $this->name
+        ! $username && $username = $this->name
             ?: $this->username
                 ?: $this->nickname
-                    ?: md5((string)$this->getKey());
+                    ?: md5((string) $this->getKey());
 
-        return AvatarUtils::generateAvatar(username: $username, size: (int)$size)->toDataUri();
+        return AvatarUtils::generateAvatar(username: $username, size: (int) $size)->toDataUri();
     }
 
     public function setAvatarAttribute($value): void
